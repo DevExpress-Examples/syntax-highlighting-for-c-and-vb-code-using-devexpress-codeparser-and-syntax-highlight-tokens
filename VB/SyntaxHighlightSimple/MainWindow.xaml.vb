6717b7279f145b9b59234e3480fec62584c70af3
DevExpressExampleBot <fakeEmail>
2023-09-27 14:27:46 +0400

    Source auto update [skip ci]

--- VB/SyntaxHighlightSimple/MainWindow.xaml.vb
+++ VB/SyntaxHighlightSimple/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports System.Diagnostics
 Imports System.Drawing
 Imports DevExpress.CodeParser
@@ -10,25 +10,25 @@ Imports DevExpress.LookAndFeel
 Imports DevExpress.Skins
 Imports DevExpress.Xpf.Core
 Imports System.IO
-Imports System
 Imports DevExpress.XtraRichEdit
 
 Namespace SyntaxHighlightSimple
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits ThemedWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub richEditControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
             ' Use service substitution to register a custom service that implements highlighting.
-            richEditControl1.ReplaceService(Of ISyntaxHighlightService)(New MySyntaxHighlightService(richEditControl1))
-            Dim path As String = "MainWindow.xaml.vb"
-            richEditControl1.LoadDocument(path, DocumentFormat.PlainText)
+            Me.richEditControl1.ReplaceService(Of ISyntaxHighlightService)(New MySyntaxHighlightService(Me.richEditControl1))
+            Dim path As String = "MainWindow.xaml.cs"
+            Me.richEditControl1.LoadDocument(path, DocumentFormat.PlainText)
         End Sub
     End Class
 
@@ -36,48 +36,47 @@ Namespace SyntaxHighlightSimple
         Implements ISyntaxHighlightService
 
         Private ReadOnly syntaxEditor As RichEditControl
+
         Private syntaxColors As SyntaxColors
+
         Private commentProperties As SyntaxHighlightProperties
+
         Private keywordProperties As SyntaxHighlightProperties
+
         Private stringProperties As SyntaxHighlightProperties
+
         Private xmlCommentProperties As SyntaxHighlightProperties
+
         Private textProperties As SyntaxHighlightProperties
 
         Public Sub New(ByVal syntaxEditor As RichEditControl)
             Me.syntaxEditor = syntaxEditor
             syntaxColors = New SyntaxColors(UserLookAndFeel.Default)
-
         End Sub
 
         Private Sub HighlightSyntax(ByVal tokens As TokenCollection)
             commentProperties = New SyntaxHighlightProperties()
             commentProperties.ForeColor = syntaxColors.CommentColor
-
             keywordProperties = New SyntaxHighlightProperties()
             keywordProperties.ForeColor = syntaxColors.KeywordColor
-
             stringProperties = New SyntaxHighlightProperties()
             stringProperties.ForeColor = syntaxColors.StringColor
-
             xmlCommentProperties = New SyntaxHighlightProperties()
             xmlCommentProperties.ForeColor = syntaxColors.XmlCommentColor
-
             textProperties = New SyntaxHighlightProperties()
             textProperties.ForeColor = syntaxColors.TextColor
-
-            If tokens Is Nothing OrElse tokens.Count = 0 Then
-                Return
-            End If
-
+            If tokens Is Nothing OrElse tokens.Count = 0 Then Return
             Dim document As Document = syntaxEditor.Document
             Dim cp As CharacterProperties = document.BeginUpdateCharacters(0, 1)
-            Dim syntaxTokens As New List(Of SyntaxHighlightToken)(tokens.Count)
+            Dim syntaxTokens As List(Of SyntaxHighlightToken) = New List(Of SyntaxHighlightToken)(tokens.Count)
             For Each token As Token In tokens
                 HighlightCategorizedToken(CType(token, CategorizedToken), syntaxTokens)
-            Next token
+            Next
+
             document.ApplySyntaxHighlight(syntaxTokens)
             document.EndUpdateCharacters(cp)
         End Sub
+
         Private Sub HighlightCategorizedToken(ByVal token As CategorizedToken, ByVal syntaxTokens As List(Of SyntaxHighlightToken))
             Dim backColor As Color = syntaxEditor.ActiveView.BackColor
             Dim category As TokenCategory = token.Category
@@ -93,18 +92,14 @@ Namespace SyntaxHighlightSimple
                 syntaxTokens.Add(SetTokenColor(token, textProperties, backColor))
             End If
         End Sub
+
         Private Function SetTokenColor(ByVal token As Token, ByVal foreColor As SyntaxHighlightProperties, ByVal backColor As Color) As SyntaxHighlightToken
-            If syntaxEditor.Document.Paragraphs.Count < token.Range.Start.Line Then
-                Return Nothing
-            End If
+            If syntaxEditor.Document.Paragraphs.Count < token.Range.Start.Line Then Return Nothing
             Dim paragraphStart As Integer = DocumentHelper.GetParagraphStart(syntaxEditor.Document.Paragraphs(token.Range.Start.Line - 1))
             Dim tokenStart As Integer = paragraphStart + token.Range.Start.Offset - 1
-            If token.Range.End.Line <> token.Range.Start.Line Then
-                paragraphStart = DocumentHelper.GetParagraphStart(syntaxEditor.Document.Paragraphs(token.Range.End.Line - 1))
-            End If
-
+            If token.Range.End.Line <> token.Range.Start.Line Then paragraphStart = DocumentHelper.GetParagraphStart(syntaxEditor.Document.Paragraphs(token.Range.End.Line - 1))
             Dim tokenEnd As Integer = paragraphStart + token.Range.End.Offset - 1
-            Debug.Assert(tokenEnd > tokenStart)
+            Call Debug.Assert(tokenEnd > tokenStart)
             Return New SyntaxHighlightToken(tokenStart, tokenEnd - tokenStart, foreColor)
         End Function
 
@@ -115,9 +110,7 @@ Namespace SyntaxHighlightSimple
             Dim ext As String = Path.GetExtension(syntaxEditor.Options.DocumentSaveOptions.CurrentFileName)
             Dim lang_ID As ParserLanguageID = ParserLanguage.FromFileExtension(ext)
             ' Do not parse HTML or XML.
-            If lang_ID = ParserLanguageID.Html OrElse lang_ID = ParserLanguageID.Xml OrElse lang_ID = ParserLanguageID.None Then
-                Return
-            End If
+            If lang_ID = ParserLanguageID.Html OrElse lang_ID = ParserLanguageID.Xml OrElse lang_ID = ParserLanguageID.None Then Return
             ' Use DevExpress.CodeParser to parse text into tokens.
             Dim tokenHelper As ITokenCategoryHelper = TokenCategoryHelperFactory.CreateHelper(lang_ID)
             Dim highlightTokens As TokenCollection
@@ -128,61 +121,71 @@ Namespace SyntaxHighlightSimple
         Public Sub ForceExecute() Implements ISyntaxHighlightService.ForceExecute
             Execute()
         End Sub
-
-#End Region ' #ISyntaxHighlightServiceMembers
+#End Region  ' #ISyntaxHighlightServiceMembers
     End Class
+
     ''' <summary>
     '''  This class provides colors to highlight the tokens.
     ''' </summary>
     Public Class SyntaxColors
-        Private Shared ReadOnly Property DefaultCommentColor() As Color
+
+        Private Shared ReadOnly Property DefaultCommentColor As Color
             Get
                 Return Color.Green
             End Get
         End Property
-        Private Shared ReadOnly Property DefaultKeywordColor() As Color
+
+        Private Shared ReadOnly Property DefaultKeywordColor As Color
             Get
                 Return Color.Blue
             End Get
         End Property
-        Private Shared ReadOnly Property DefaultStringColor() As Color
+
+        Private Shared ReadOnly Property DefaultStringColor As Color
             Get
                 Return Color.Brown
             End Get
         End Property
-        Private Shared ReadOnly Property DefaultXmlCommentColor() As Color
+
+        Private Shared ReadOnly Property DefaultXmlCommentColor As Color
             Get
                 Return Color.Gray
             End Get
         End Property
-        Private Shared ReadOnly Property DefaultTextColor() As Color
+
+        Private Shared ReadOnly Property DefaultTextColor As Color
             Get
                 Return Color.Black
             End Get
         End Property
+
         Private lookAndFeel As UserLookAndFeel
 
-        Public ReadOnly Property CommentColor() As Color
+        Public ReadOnly Property CommentColor As Color
             Get
                 Return GetCommonColorByName(CommonSkins.SkinInformationColor, DefaultCommentColor)
             End Get
         End Property
-        Public ReadOnly Property KeywordColor() As Color
+
+        Public ReadOnly Property KeywordColor As Color
             Get
                 Return GetCommonColorByName(CommonSkins.SkinQuestionColor, DefaultKeywordColor)
             End Get
         End Property
-        Public ReadOnly Property TextColor() As Color
+
+        Public ReadOnly Property TextColor As Color
             Get
                 Return GetCommonColorByName(CommonColors.WindowText, DefaultTextColor)
             End Get
         End Property
-        Public ReadOnly Property XmlCommentColor() As Color
+
+        Public ReadOnly Property XmlCommentColor As Color
             Get
                 Return GetCommonColorByName(CommonColors.DisabledText, DefaultXmlCommentColor)
             End Get
         End Property
-        Public ReadOnly Property StringColor() As Color
+
+        Public ReadOnly Property StringColor As Color
             Get
                 Return GetCommonColorByName(CommonSkins.SkinWarningColor, DefaultStringColor)
             End Get
@@ -194,9 +197,7 @@ Namespace SyntaxHighlightSimple
 
         Private Function GetCommonColorByName(ByVal colorName As String, ByVal defaultColor As Color) As Color
             Dim skin As Skin = CommonSkins.GetSkin(lookAndFeel)
-            If skin Is Nothing Then
-                Return defaultColor
-            End If
+            If skin Is Nothing Then Return defaultColor
             Return skin.Colors(colorName)
         End Function
     End Class
